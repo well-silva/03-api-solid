4a813f16fd309c85e55d16d2479f3748b3bd7386
Wellington Silva <well.silvaa97@gmail.com>
2024-01-31 04:46:14 -0300

    feat: add validation env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from './app';
+import { env } from './env';
 
 app.listen({
-	port: 3000,
+	port: env.PORT,
 	host: '0.0.0.0'
 }).then(() => console.log('Server is listening on port 3000'));
