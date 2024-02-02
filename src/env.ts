0c7376121a302e6d81e0b7b5e399725665c8a65f
Wellington Silva <well.silvaa97@gmail.com>
2024-02-02 04:50:37 -0300

    refactor: add config eslint rocketseat

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,20 @@
-import 'dotenv';
+import 'dotenv'
 
-import { z } from 'zod';
+import { z } from 'zod'
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-	PORT: z.coerce.number().default(3000),
-});
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  PORT: z.coerce.number().default(3000),
+})
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-	console.error('Invalid environment variables', _env.error.format());
+  console.error('Invalid environment variables', _env.error.format())
 
-	throw new Error('Invalid environment variables');
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from './app';
-import { env } from './env';
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-	port: env.PORT,
-	host: '0.0.0.0'
-}).then(() => console.log('Server is listening on port 3000'));
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => console.log(`Server is listening on port ${env.PORT}`))
