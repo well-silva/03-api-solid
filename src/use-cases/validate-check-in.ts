c4fc3ff75de86c9532b72d23a7fb29121ebd2826
Wellington Silva <well.silvaa97@gmail.com>
2024-03-06 19:46:04 -0300

    feat: add use case and test of validate check-in

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,43 @@
+import { CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
+
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkinsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkinsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.createdAt,
+      'minute',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new LateCheckInValidationError()
+
+    checkIn.validated_at = new Date()
+
+    await this.checkinsRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
