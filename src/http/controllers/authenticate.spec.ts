92ec2138276ebb8194bd0e035e25cb69126cc465
Wellington Silva <well.silvaa97@gmail.com>
2024-03-12 18:29:56 -0300

    test: add test e2e for authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('test', () => {})
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate a user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
